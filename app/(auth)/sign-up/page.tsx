b9a422dc7af5b692e826e15d366a2688ecc0b4c6
sm-muhaimen <amuhaimenpro829@gmail.com>
2025-09-16 12:05:13 +0600

    sign up form done

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,40 +1,106 @@
- import React from 'react'
- 
- export default function page() {
-   return (
-     <div className=' main_bg h-screen flex justify-center items-center'> 
-     <div className=' role_question_bg py-12 px-[42px] w-[700px]  rounded-[20px]'>
-        <h2 className=' text-white text-[32px] font-semibold mb-12'>Sign Up</h2>
-        <div className=' space-y-6'>
-          {/* full name */}
-        <div className=' flex flex-col w-full'>
-          <label htmlFor="fullName" className=' mb-3 text-[#E9E9EA] text-lg font-medium'>Full Name</label>
-          <input type="text" name="" id="fullName" placeholder='Enter Your full name' className=' w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium' />
-        </div>
-        {/* email */}
-        <div className=' flex flex-col w-full'>
-          <label htmlFor="email" className=' mb-3 text-[#E9E9EA] text-lg font-medium'>Email</label>
-          <input type="email" name="" id="email" placeholder='Enter your email' className=' w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium' />
-        </div>
-        {/* password */}
-        <div className=' flex flex-col w-full'>
-          <label htmlFor="password" className=' mb-3 text-[#E9E9EA] text-lg font-medium'>Password</label>
-          <input type="password" name="" id="" placeholder='Enter your password' className=' w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium' />
-        </div>
-        {/* confirm password */}
-        <div className=' flex flex-col w-full'>
-          <label htmlFor="cPassword" className=' mb-3 text-[#E9E9EA] text-lg font-medium'>Confirm Password</label>
-          <input type="password" name="" id="cPassword" placeholder='Enter your confirm password' className=' w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium' />
-        </div>
-        {/* phone */}
-        <div className=' flex flex-col w-full'>
-          <label htmlFor="phone" className=' mb-3 text-[#E9E9EA] text-lg font-medium'>Phone Number</label>
-          <input type="text" name="" id="phone" placeholder='Enter your phone number' className='w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium' />
-        </div>
+import React from "react";
 
+export default function page() {
+  return (
+    <div className=" main_bg h-screen flex justify-center items-center">
+      <div className=" role_question_bg py-12 px-[42px] w-[700px]  rounded-[20px]">
+        <h2 className=" text-white text-[32px] font-semibold mb-12">Sign Up</h2>
+        <div className=" space-y-6">
+          {/* full name */}
+          <div className=" flex flex-col w-full">
+            <label
+              htmlFor="fullName"
+              className=" mb-3 text-[#E9E9EA] text-lg font-medium"
+            >
+              Full Name
+            </label>
+            <input
+              type="text"
+              name=""
+              id="fullName"
+              placeholder="Enter Your full name"
+              className=" w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium"
+            />
+          </div>
+          {/* email */}
+          <div className=" flex flex-col w-full">
+            <label
+              htmlFor="email"
+              className=" mb-3 text-[#E9E9EA] text-lg font-medium"
+            >
+              Email
+            </label>
+            <input
+              type="email"
+              name=""
+              id="email"
+              placeholder="Enter your email"
+              className=" w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium"
+            />
+          </div>
+          {/* password */}
+          <div className=" flex flex-col w-full">
+            <label
+              htmlFor="password"
+              className=" mb-3 text-[#E9E9EA] text-lg font-medium"
+            >
+              Password
+            </label>
+            <input
+              type="password"
+              name=""
+              id=""
+              placeholder="Enter your password"
+              className=" w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium"
+            />
+          </div>
+          {/* confirm password */}
+          <div className=" flex flex-col w-full">
+            <label
+              htmlFor="cPassword"
+              className=" mb-3 text-[#E9E9EA] text-lg font-medium"
+            >
+              Confirm Password
+            </label>
+            <input
+              type="password"
+              name=""
+              id="cPassword"
+              placeholder="Enter your confirm password"
+              className=" w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium"
+            />
+          </div>
+          {/* phone */}
+          <div className=" flex flex-col w-full">
+            <label
+              htmlFor="phone"
+              className=" mb-3 text-[#E9E9EA] text-lg font-medium"
+            >
+              Phone Number
+            </label>
+            <input
+              type="text"
+              name=""
+              id="phone"
+              placeholder="Enter your phone number"
+              className="w-full py-3 px-6 border border-[#393939] rounded-xl placeholder:text-[#D2D2D5] placeholder:font-medium "
+            />
+          </div>
+          {/* terms and privacy */}
+          <div className="flex items-center gap-2 ">
+            <input type="checkbox" name="" id="term" className=" cursor-pointer" />
+            <label htmlFor="term" className="  text-[#E9E9EA] text-base  ">
+              I agree to the Terms and Privacy Policy
+            </label>
+          </div>
+
+          <input
+            type="submit"
+            value="Submit"
+            className=" auth_btn_bg w-full py-4 px-5 text-white text-lg font-semibold rounded-xl cursor-pointer"
+          />
         </div>
-     </div>
-     </div>
-   )
- }
- 
+      </div>
+    </div>
+  );
+}
