a125d5fc30691e950c39b9b06faa8e44933f9430
sm-muhaimen <amuhaimenpro829@gmail.com>
2025-09-18 11:58:51 +0600

    contact cards added

--- app/(dashboard)/dashboard/contacts/_components/ContactCard.tsx
+++ app/(dashboard)/dashboard/contacts/_components/ContactCard.tsx
@@ -2,6 +2,35 @@ import React from 'react'
 
 export default function ContactCard() {
   return (
-    <div>ContactCard</div>
+    <div className='  bg-[#1c1c1a] p-5  grid grid-cols-3 gap-6 mt-5'>
+
+    <div className='   bg-[#202020] p-5  rounded-2xl border border-[#2c6a4b]'>
+        <h3 className=' text-2xl text-white font-semibold mb-2'>Acme Redesign</h3>
+        <p className=' text-lg text-white mb-4'>Jan 10 - Apr 30</p>
+        <p className=' contact_card_timeline_bg py-2.5 px-3  border border-[#2c6a4b] rounded-[10px] inline-block text-white text-base font-semibold mb-3'>Update timeline</p>
+        <div>
+            <p className=' text-white text-base mb-6'>Received a message</p>
+        </div>
+        <h4 className=' text-[#21AF68] text-2xl font-semibold'>$12.500</h4>
+    </div>
+    <div className='   bg-[#202020] p-5  rounded-2xl border border-[#2c6a4b]'>
+        <h3 className=' text-2xl text-white font-semibold mb-2'>Acme Redesign</h3>
+        <p className=' text-lg text-white mb-4'>Jan 10 - Apr 30</p>
+        <p className=' contact_card_timeline_bg py-2.5 px-3  border border-[#2c6a4b] rounded-[10px] inline-block text-white text-base font-semibold mb-3'>Update timeline</p>
+        <div>
+            <p className=' text-white text-base mb-6'>Received a message</p>
+        </div>
+        <h4 className=' text-[#21AF68] text-2xl font-semibold'>$12.500</h4>
+    </div>
+    <div className='   bg-[#202020] p-5  rounded-2xl border border-[#2c6a4b]'>
+        <h3 className=' text-2xl text-white font-semibold mb-2'>Acme Redesign</h3>
+        <p className=' text-lg text-white mb-4'>Jan 10 - Apr 30</p>
+        <p className=' contact_card_timeline_bg py-2.5 px-3  border border-[#2c6a4b] rounded-[10px] inline-block text-white text-base font-semibold mb-3'>Update timeline</p>
+        <div>
+            <p className=' text-white text-base mb-6'>Received a message</p>
+        </div>
+        <h4 className=' text-[#21AF68] text-2xl font-semibold'>$12.500</h4>
+    </div>
+    </div>
   )
 }
--- app/(dashboard)/dashboard/contacts/page.tsx
+++ app/(dashboard)/dashboard/contacts/page.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
+import ContactCard from './_components/ContactCard'
 
 export default function page() {
   return (
     <div>
+        <div>
         <h2 className='text-white text-[32px] font-medium'>Contracts Overview</h2>
         <p className=' text-[#A5A5AB] text-sm font-medium  mt-3'>Apr 22, 2024</p>
+
+        </div>
+        <ContactCard/>
     </div>
   )
 }
