510d6160d7e0557159541d05c0b90bd2e9e024e5
sm-muhaimen <amuhaimenpro829@gmail.com>
2025-09-21 16:09:33 +0600

    bottom part left side done

--- app/(dashboard)/dashboard/ui-designer-profile/_components/UIProfileBottom.tsx
+++ app/(dashboard)/dashboard/ui-designer-profile/_components/UIProfileBottom.tsx
@@ -10,7 +10,8 @@ import Image from "next/image";
 
 export default function UIProfileBottom() {
   return (
-    <div className=" flex items-start gap-4 mt-6">
+    <div className=" flex flex-col lg:flex-row items-start gap-4 mt-6">
+      {/* =========================== one =========================== */}
       <div className=" bg-[#161616] py-4 px-6 flex-1 text-white rounded-3xl space-y-6">
         {/* one */}
         <div>
@@ -104,21 +105,85 @@ export default function UIProfileBottom() {
             </div>
             <div className="py-4 px-3 common_btn_bg border border-[#276d4a] rounded-[10px] text-center flex-1">
               <h2 className="text-white text-base font-semibold">
-                Certified UX <br /> Designer
+                Google UX <br /> Design
               </h2>
               <p className="text-[#E9E9EA] text-sm">2025</p>
             </div>
             <div className="py-4 px-3 common_btn_bg border border-[#276d4a] rounded-[10px] text-center flex-1">
               <h2 className="text-white text-base font-semibold">
-                Certified UX <br /> Designer
+                Accessibility <br /> Certification
               </h2>
               <p className="text-[#E9E9EA] text-sm">2025</p>
             </div>
           </div>
         </div>
       </div>
+      {/*========================== two =============================== */}
       <div className=" bg-[#161616] py-4 px-6  flex-1 text-white rounded-3xl">
-        2
+        <div>
+          <h2 className=" text-xl text-white font-semibold">Work Experience</h2>
+          {/* one */}
+          <div className=" mt-2">
+            <h3 className=" text-lg text-white font-semibold">
+              Senior Ul UX Designer
+            </h3>
+            <p className=" text-[#D2D2D5] text-base ">
+              Ultimate Wedding Planner, Nasshyilllg II. / May 2016- Present
+            </p>
+            <ul className=" list-disc list-outside ml-6 space-y-1 mt-2">
+              <li className=" text-[#D2D2D5] text-base">
+                Oversee all aspects of UX design for making Llj design/UM video,
+                or <br /> mobile design team experience on launch
+              </li>
+              <li className=" text-[#D2D2D5] text-base">
+                Manage. application develops developers already well on view the <br />
+                mobile   app. creating a five-star rated mobile app based on <br />
+                development
+              </li>
+              <li className=" text-[#D2D2D5] text-base">
+                Conduct market research on user needs and add redefine
+                integration <br /> and treasures for improving ove sleeps and
+                customization mobile <br /> application a leveler note sites of launch
+              </li>
+            </ul>
+          </div>
+          <div className=" mt-2">
+            <h3 className=" text-lg text-white font-semibold">skills</h3>
+
+            <ul className=" list-disc list-outside ml-6 space-y-1 mt-2">
+              <li className=" text-[#D2D2D5] text-base">
+                UX design at S).a i or Engiineeering/, Nlashidv, TN
+              </li>
+              <li className=" text-[#D2D2D5] text-base">
+                Mobile application development
+              </li>
+              <li className=" text-[#D2D2D5] text-base">
+                User-centered design
+              </li>
+              <li className=" text-[#D2D2D5] text-base">Wireframing</li>
+              <li className=" text-[#D2D2D5] text-base">
+                Cross-functional leadership
+              </li>
+            </ul>
+          </div>
+          <div className=" mt-2">
+            <h3 className=" text-lg text-white font-semibold">
+              Certifications
+            </h3>
+
+            <ul className=" list-disc list-outside ml-6 space-y-1 mt-2">
+              <li className=" text-[#D2D2D5] text-base">
+                Certified User Experience Analyst (CXA) <br /> I April 2019
+              </li>
+              <li className=" text-[#D2D2D5] text-base">
+                NN/g UX Certification <br />I August 2017
+              </li>
+              <li className=" text-[#D2D2D5] text-base">
+                Google UX Deign Professional Certificate <br /> I October 2015
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </div>
   );
